722c917a19496087171ca5942c0842f86853f423
Luffy2025 <bestparty2025@gmail.com>
2024-11-25 17:51:22 +0800

    feature: update Mutex to RwLock

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,10 +1,10 @@
 use anyhow::Result;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    pub data: Arc<Mutex<HashMap<String, i64>>>,
+    pub data: Arc<RwLock<HashMap<String, i64>>>,
 }
 
 impl Default for Metrics {
@@ -16,14 +16,14 @@ impl Default for Metrics {
 impl Metrics {
     pub fn new() -> Self {
         Metrics {
-            data: Arc::new(Mutex::new(HashMap::new())),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
         let mut data = self
             .data
-            .lock()
+            .write()
             .map_err(|e| anyhow::anyhow!(e.to_string()))?;
 
         let counter = data.entry(key.into()).or_insert(0);
@@ -35,7 +35,7 @@ impl Metrics {
     pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
         Ok(self
             .data
-            .lock()
+            .read()
             .map_err(|e| anyhow::anyhow!(e.to_string()))?
             .clone())
     }
