8c2c322b878dbd9d6a2bceb4e20b1e9e1b2124d2
Luffy2025 <bestparty2025@gmail.com>
2024-11-25 17:46:05 +0800

    feature: add metrics to learning sync::mutex

---
+++ examples/metrics.rs
@@ -0,0 +1,51 @@
+use anyhow::Result;
+use r_concurrency::Metrics;
+use rand::Rng;
+use std::thread;
+use std::time::Duration;
+
+const N: usize = 2;
+const M: usize = 4;
+
+fn main() -> Result<()> {
+    let metrics = Metrics::new();
+    metrics.inc("requests.prepare")?;
+    metrics.inc("requests.prepare")?;
+    println!("{:?}", metrics.snapshot());
+
+    for idx in 0..N {
+        task_worker(idx, metrics.clone());
+    }
+    for _ in 0..M {
+        request_worker(metrics.clone());
+    }
+
+    loop {
+        thread::sleep(Duration::from_secs(2));
+        println!("{:?}", metrics.snapshot());
+    }
+}
+
+fn task_worker(idx: usize, metrics: Metrics) {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(0..1000)));
+            metrics.inc(format!("thread.worker.{}", idx))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+}
+
+fn request_worker(metrics: Metrics) {
+    thread::spawn(move || {
+        for _ in 0..10 {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(500..800)));
+            let page = rng.gen_range(6..8);
+            metrics.inc(format!("requests.page.{}", page))?;
+        }
+        Ok::<_, anyhow::Error>(())
+    });
+}
